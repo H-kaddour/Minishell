11c55f9af3edff7dc8a0daafb0bc804e91aadd41
Hicham Kaddouri <hkaddour@e1r6p1.1337.ma>
2022-10-04 09:43:03 +0100

    push comment of pipeline

--- execution/pipeline.c
+++ execution/pipeline.c
@@ -6,66 +6,109 @@
 /*   By: hkaddour <hkaddour@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 22:02:51 by hkaddour          #+#    #+#             */
-/*   Updated: 2022/10/03 15:17:58 by hkaddour         ###   ########.fr       */
+/*   Updated: 2022/10/04 09:42:40 by hkaddour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-//void  run(t_data *data, char *cmd)
+
+
+//void  pipeline(t_data *data)
 //{
-//  char *c1;
-//  char *ll;
-//  char **sp;
+//	int fd[2];
+//	pid_t pid;
+//	t_cmd	*cmd;
+//	int fdd = 0;
+//	int	*tab_fd;
+//	int	i;
+//	int	j;
 //
-//  if (cmd[0] == 'l')
-//  {
-//    c1 = ft_strjoin("/bin/", cmd);
-//    ll = "ls -la";
-//    sp = ft_split(ll, ' ');
-//  }
-//  else if (cmd[0] == 'g')
-//  {
-//    c1 = ft_strjoin("/usr/bin/", cmd);
-//    ll = "grep gi";
-//    sp = ft_split(ll, ' ');
-//  }
-//  else if (cmd[0] == 'w')
-//  {
-//    c1 = ft_strjoin("/usr/bin/", cmd);
-//    ll = "wc -l";
-//    sp = ft_split(ll, ' ');
-//  }
-//  execve(c1, sp, data->env);
+//	i = 0;
+//	tab_fd = malloc(sizeof(int) * 4);
+//	pipe(fd);
+//	tab_fd[0] = fd[0];
+//	tab_fd[1] = fd[1];
+//	pipe(fd);
+//	tab_fd[2] = fd[0];
+//	tab_fd[3] = fd[1];
+//	//while (i < 4)
+//	//{
+//	//	printf("%d\n", tab_fd[i]);
+//	//	i++;
+//	//}
+//	i = 0;
+//	j = 0;
+//	cmd = data->v_cmd;
+//	while (cmd)
+//	{
+//		if (pipe(fd) == 0)
+//		{
+//			printf("pipe = in=%d --- out=%d\n", fd[0], fd[1]);
+//		}
+//		if ((pid = fork()) == -1)
+//		{
+//			perror("fork");
+//			exit(1);
+//		}
+//		else if (pid == 0)
+//		{
+//			printf("|child: cmd = %s\n", cmd->cmd[0]);
+//			printf("	-->child:in = %d		out = %d\n", fdd, tab_fd[i+1]);
+//			//printf("	-->child:in = %d		out = %d	p fdd= %d\n", tab_fd[i], tab_fd[i+1], fdd);
+//			dup2(fdd, 0);
+//			if (cmd->next)
+//			{
+//				dup2(tab_fd[i+1], 1);
+//			}
+//			close(fd[i]);
+//			//printf("child: %s close %d\n", cmd->cmd[0], fd[0]);
+//			//close(fd[1]);
+//			execute_sys_cmd(data, cmd);
+//			//exit(1);
+//		}
+//		else
+//		{
+//			wait(NULL);
+//			close(fd[i + 1]);
+//			//printf("parent: %s close %d\n", cmd->cmd[0], fd[1]);
+//			fdd = tab_fd[i];
+//			//while(1);
+//			printf("|parent: cmd = %s\n", cmd->cmd[0]);
+//			printf("	-->child:in = %d		out = %d\n", fdd, tab_fd[i+1]);
+//			//printf("parent: cmd= %s		in = %d		out = %d\n", cmd->cmd[0], fd[0], fd[1]);
+//			i += 2;
+//			cmd = cmd->next;
+//		}
+//	}
 //}
 
 void  pipeline(t_data *data)
 {
-  //int   **pipe_fds;
-
-  //pipe_fds = malloc(sizeof(int *) * 2);
   t_cmd *trav;
+  t_cmd *p_trav;
   int   i;
+  int   j;
   //int   j;
   int   pid;
   int   fd[2];
   //char  rd[10] = {0};
   //int   fd_hold;
+  //int   *fds_tab;
 
   i = 0;
   trav = data->v_cmd;
   //trav->tab_pipe = malloc(sizeof(int *) * data->size_cmd - 1);
+  //fds_tab = malloc(sizeof(int) * (data->size_cmd - 1) * 2);
   while (trav->next)
   {
     i = 0;
+    j = 0;
     if (pipe(fd))
     {
 		  printf("error in pipe\n");
       return ;
     }
-    //pipe_fds[i] = malloc(sizeof(int) * 2);
-    //pipe_fds[i][j++] = fd[1];
-    //pipe_fds[i][j] = fd[0];
     trav->tab_pipe = malloc(sizeof(int) * 2);
     trav->tab_pipe[i++] = fd[1];
     trav->tab_pipe[i] = fd[0];
@@ -75,25 +118,25 @@ void  pipeline(t_data *data)
     //printf("%d  %d\n", pipe_fds[i][0], pipe_fds[i][1]);
     //i++;
   }
-  //j = 0;
+  //old_algo
   i = 0;
   trav->tab_pipe = malloc(sizeof(int) * 2);
   trav->tab_pipe[i++] = fd[1];
   trav->tab_pipe[i] = fd[0];
 
   //print the files in tab_pipe
-  //trav = data->v_cmd;
-  //while (trav)
-  //{
-  //  i = 0;
-  //  while (i < 2)
-  //  {
-  //    printf("%d ", trav->tab_pipe[i]);
-  //    i++;
-  //  }
-  //  printf("\n");
-  //  trav = trav->next;
-  //}
+  trav = data->v_cmd;
+  while (trav)
+  {
+    i = 0;
+    while (i < 2)
+    {
+      printf("%d ", trav->tab_pipe[i]);
+      i++;
+    }
+    printf("\n");
+    trav = trav->next;
+  }
 
   //**trav = data->v_cmd;
   //**while (trav)
@@ -103,24 +146,15 @@ void  pipeline(t_data *data)
   //**  trav = trav->next;
   //**}
   //printf("hey\n");
-  t_cmd *p_trav;
-  i = 0;
 
+  //*t_cmd *p_trav;
+  //*i = 0;
+
+  i = 0;
   trav = data->v_cmd;
-  //p_trav = data->v_cmd;
-  //**printf("\n");
-  //**printf("%d  ", trav->tab_pipe[0]);
-  //**printf("%d", trav->tab_pipe[1]);
-  //**trav = trav->next;
-  //**printf("\n");
-  //**printf("%d  ", trav->tab_pipe[0]);
-  //**printf("%d", trav->tab_pipe[1]);
-  //i = 0;
+  p_trav = 0;
   while (trav)
   {
-    //pipe(fd);
-    //printf("cmd = %s    in = %d    out = %d\n", trav->cmd[0], trav->f_in, trav->f_out);
-    //printf("cmd = %s    in = %d    out = %d\n", trav->cmd[0], trav->f_in, trav->f_out);
     pid = fork();
     if (pid < 0)
     {
@@ -129,85 +163,132 @@ void  pipeline(t_data *data)
     }
     if (pid == 0)
     {
-      //if (i == 0)
-      //  close(trav->f_in);
-      //if (!trav->next)
-      //  close(trav->f_out);
-      printf("%d\n", i);
-      printf("%s\n", trav->cmd[0]);
+			//printf("|child: cmd = %s\n", trav->cmd[0]);
+			//printf("	-->child:in = %d		out = %d\n", trav->f_in, trav->f_out);
       dup2(trav->f_out, STDOUT_FILENO);
       dup2(trav->f_in, STDIN_FILENO);
-      //close(fd[0]);
-      //close(fd[1]);
-        close(trav->tab_pipe[0]);
-        close(trav->tab_pipe[1]);
-      //if (trav->f_in > 0)
-      //  close(trav->f_in);
-      //if (trav->f_out > 1)
-      //  close(trav->f_out);
-      //if (trav->cmd[0][0] == 'w')
-
-      //if (trav->cmd[0][0] == 'w')
-      //{
-      //  printf("here\n");
-      //  while (1);
-      //}
+      close(trav->tab_pipe[0]);
+      close(trav->tab_pipe[1]);
       execute_sys_cmd(data, trav);
-      //close(trav->f_in);
-      //close(trav->f_out);
     }
-    //if (pid >= 1)
-    //{
-    //  close(trav->tab_pipe[0]);
-    //  close(trav->tab_pipe[1]);
-    //  waitpid(pid, NULL, 0);
-    //  //trav = trav->next;
-    //}
-    trav = trav->next;
-    i++;
-  }
-
-  //trav = data->v_cmd;
-  //while(p_trav)
-  p_trav = data->v_cmd;
-  //while(1)
-  //while(p_trav)
-  //while(1)
-  while(p_trav)
-  {
-    //close(fd[0]);
-    //close(fd[1]);
-      //printf("in parent %d\n", i);
-      printf("in parent %s\n", p_trav->cmd[0]);
-      //printf("in parent dial trav %s\n", trav->cmd[0]);
-      //if (p_trav->next)
-      //{
-        printf("parent closing file\n");
-        close(p_trav->tab_pipe[0]);
-        close(p_trav->tab_pipe[1]);
-      //}
-    //waitpid(trav->pid, NULL, 0);
-    //wait(0);
-    //trav = trav->next;
-    //if (wait(0))
-    //if (waitpid(-1, NULL, 0) == -1)
-    //waitpid(pid, NULL, 0);
-      //p_trav = p_trav->next;
-    if (waitpid(pid, 0, 0) == -1)
+    if (pid > 0)
     {
-      p_trav = p_trav->next;
-      printf("hye\n");
-      //trav = trav->next;
-      break;
+      wait(0);
+      if (p_trav)
+        close(p_trav->tab_pipe[1]);
+      close(trav->tab_pipe[0]);
+      p_trav = trav;
+      trav = trav->next;
     }
-    printf("end of loop\n");
-    //p_trav = p_trav->next;
   }
-  //printf("im out\n");
-
 }
 //pseudo code:
 //first i will make the pipes for the size_of_cmd - 1
 //loop for the cmd nodes and initialize in and out with their pipes values
 //then check if there's redirection if yes close in or out pipe in node and replace it with red files
 //make another loop on cmd nodes and start execting the cmd
+//
+//
+//
+//#include <stdlib.h>
+//#include <unistd.h>
+//#include <stdio.h>
+//
+///*
+// * loop over commands by sharing
+// * pipes.
+// */
+//static void
+//pipeline(char ***cmd)
+//{
+//	int fd[2];
+//	pid_t pid;
+//	int fdd = 0;				/* Backup */
+//
+//	while (*cmd != NULL) {
+//		pipe(fd);
+//		if ((pid = fork()) == -1) {
+//			perror("fork");
+//			exit(1);
+//		}
+//		else if (pid == 0) {
+//			dup2(fdd, 0);
+//			if (*(cmd + 1) != NULL) {
+//				dup2(fd[1], 1);
+//			}
+//			close(fd[0]);
+//			execvp((*cmd)[0], *cmd);
+//			exit(1);
+//		}
+//		else {
+//			wait(NULL); 		/* Collect childs */
+//			close(fd[1]);
+//			fdd = fd[0];
+//			cmd++;
+//		}
+//	}
+//}
+//
+///*
+// * Compute multi-pipeline based
+// * on a command list.
+// */
+//int
+//main(int argc, char *argv[])
+//{
+//	char *ls[] = {"ls", "-al", NULL};
+//	char *rev[] = {"rev", NULL};
+//	char *nl[] = {"nl", NULL};
+//	char *cat[] = {"cat", "-e", NULL};
+//	char **cmd[] = {ls, rev, nl, cat, NULL};
+//
+//	pipeline(cmd);
+//	return (0);
+//}
+//
+//
+//
+//
+//
+//	cmd = data->v_cmd;
+//	while (cmd)
+//	{
+//		//if (pipe(fd) == 0)
+//		//{
+//		//	printf("pipe = in=%d --- out=%d\n", fd[0], fd[1]);
+//		//}
+//		if ((pid = fork()) == -1)
+//		{
+//			perror("fork");
+//			exit(1);
+//		}
+//		else if (pid == 0)
+//		{
+//			printf("|child: cmd = %s\n", cmd->cmd[0]);
+//			printf("	-->child:in = %d		out = %d\n", fdd, fd[1]);
+//			//printf("	-->child:in = %d		out = %d	p fdd= %d\n", fd[0], fd[1], fdd);
+//			dup2(fdd, 0);
+//			if (cmd->next)
+//			{
+//				dup2(fd[1], 1);
+//			}
+//			close(fd[0]);
+//			printf("child: %s close %d\n", cmd->cmd[0], fd[0]);
+//			//close(fd[1]);
+//			execute_sys_cmd(data, cmd);
+//			//exit(1);
+//		}
+//		else
+//		{
+//			wait(NULL);
+//			close(fd[1]);
+//			printf("parent: %s close %d\n", cmd->cmd[0], fd[1]);
+//			fdd = fd[0];
+//			//while(1);
+//			//printf("|parent: cmd = %s\n", cmd->cmd[0]);
+//			//printf("	-->child:in = %d		out = %d\n", fdd, fd[1]);
+//			//printf("parent: cmd= %s		in = %d		out = %d\n", cmd->cmd[0], fd[0], fd[1]);
+//			cmd = cmd->next;
+//		}
+//	}
+
